527155ee724e8346e058eb077122a719b87c8829
Daniel Koics <koics.dani@gmail.com>
2016-04-10 19:02:54 +0200

    Refactor IO exercise 1
    parsing command-line arguments,
    checking directory existence
    and listing matches
    each goes to its own separate method

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -6,7 +6,35 @@ namespace SeekAndArchive
 {
     class Program
     {
-        static List<FileInfo> filesFound = new List<FileInfo>();
+        static List<FileInfo> filesFound;
+
+        static bool TryParseArgs(string[] args, out string fileName, out string directoryName)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("This application has to be run with 2 command-line arguments, respectively:");
+                Console.WriteLine("\t* a filename to search for");
+                Console.WriteLine("\t* a directory name to search in");
+                fileName = null;
+                directoryName = null;
+                return false;
+            }
+            fileName = args[0];
+            directoryName = args[1];
+            return true;
+        }
+
+        static bool TryParseDirName(string directoryName, out DirectoryInfo directory)
+        {
+            directory = new DirectoryInfo(directoryName);
+            if (!directory.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                Console.WriteLine("Directory name: " + directoryName);
+                return false;
+            }
+            return true;
+        }
 
         static void SearchRecursively(string fileName, DirectoryInfo rootDir, List<FileInfo> destFileList)
         {
@@ -24,33 +52,27 @@ namespace SeekAndArchive
             }
         }
 
-        static void Main(string[] args)
+        static void listFiles(List<FileInfo> fileList)
         {
-            if (args.Length != 2)
-            {
-                Console.WriteLine("This application has to be run with 2 command-line arguments, respectively:");
-                Console.WriteLine("\t* a filename to search for");
-                Console.WriteLine("\t* a directory name to search in");
-                return;
-            }
-            string fileName = args[0];
-            string directoryName = args[1];
-
-            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
-            if (!rootDir.Exists)
-            {
-                Console.WriteLine("The specified directory does not exist.");
-                Console.WriteLine("Directory name: " + directoryName);
-                return;
-            }
-
-            SearchRecursively(fileName, rootDir, filesFound);
-
-            Console.WriteLine("{0} files found:", filesFound.Count);
-            foreach (FileInfo file in filesFound)
+            Console.WriteLine("{0} files found:", fileList.Count);
+            foreach (FileInfo file in fileList)
             {
                 Console.WriteLine("\t" + file.FullName);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            string fileName, directoryName;
+            if (!TryParseArgs(args, out fileName, out directoryName))
+                return;
+            DirectoryInfo rootDir;
+            if (!TryParseDirName(directoryName, out rootDir))
+                return;
+
+            filesFound = new List<FileInfo>();
+            SearchRecursively(fileName, rootDir, filesFound);
+            listFiles(filesFound);
         }
     }
 }
